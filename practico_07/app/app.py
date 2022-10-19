6e13a9a7185a5da109916c1dc0b2809e02c9bc8b
Juani <juanicampora@gmail.com>
2022-10-18 21:40:35 -0300

    Lista el alta, empezado la baja

--- practico_07/app/app.py
+++ practico_07/app/app.py
@@ -1,34 +1,41 @@
+#PARA PODER HACER EL PRACTICO TUVIMOS QUE COPIAR DE LOS PRACTICOS ANTERIORES LOS ARCHIVOS Y PEGARLOS ACA
+#TAMBIEN BORRAR LOS ASSERTS QUE PROVOCABAN ERRORES
+#TUVIMOS QUE PONER EN LA CONEXION A LA BD connect_args={"check_same_thread": False} porque sino no andaba
+
+
+#ENTENDI MAL LA CONSIGNA, HAY QUE METER LOS BOTONES ADENTRO DE LA TABLA
+
+
 from flask import Flask,render_template,request,redirect,url_for
 from ejercicio_01 import Socio
-
 from capa_negocio import NegocioSocio
 
 app=Flask(__name__)
+negocio=NegocioSocio()
 
 @app.route('/')
-def principal():
-    negocio=NegocioSocio()
+def principal():    
     data=negocio.todos()
-    print('LO QUE SALE ES')
-    print(data)
-    return render_template('principal.html')
+    return render_template('principal.html',data=data)
 
 @app.route('/alta')
 def pagalta():
     return render_template('pagalta.html')
+
 @app.route('/altaenv', methods=['post'])
 def altacontacto():
-    print(request.form['dni'])
-    print(request.form['nombre'])
-    print(request.form['apellido'])
+    dnialta=request.form['dni']
+    nombrealta=request.form['nombre']
+    apellidoalta=request.form['apellido']
+    socio=Socio(dni=dnialta, nombre=nombrealta, apellido=apellidoalta)
+    exito = negocio.alta(socio)
+    if exito:
+        print ("Alta correcta")
     return redirect(url_for('principal'))
-    #negocio=NegocioSocio()
-    #socio=Socio(dni=12345678, nombre='Juan', apellido='Lopez')
-    #exito = negocio.alta(socio)
-    #if exito:
-    #    print ("Alta correcta")
     
-    
+@app.route('/baja')
+def pagbaja():
+    return render_template('pagbaja.html')    
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
